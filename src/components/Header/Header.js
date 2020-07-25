6b43a963dd999a79e0d086c9a16fe29b6ee5812c
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-25 18:48:53 -0400

    basic header setup..

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import Navigation from '../Navigation/Navigation';
+
+export default function Header() {
+  return (
+    <header>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"> 
+            <Navbar.Brand href="#home">Ankit Mudvari</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Navigation />
+            </Navbar.Collapse>
+        </Navbar>
+    </header>
+  );
+}
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+
+export default function Navigation() {
+  return (
+<Nav defaultActiveKey="/home" as="ul">
+    <Link 
+        to='/'
+        className={
+            useLocation.pathname === '/' === 'About' ? 'nav-link-active' : 'nav-link'
+        }
+        >About
+    </Link>
+    <Link 
+        to='/portfolio'
+        className={
+            useLocation.pathname === '/portfolio' ? 'nav-link-active' : 'nav-link'
+        }
+        >Portfolio
+    </Link>
+    <Link 
+        to='/contact'
+        className={
+            useLocation.pathname === '/contact' ? 'nav-link-active' : 'nav-link'
+        }
+        >Contact Me
+    </Link>
+</Nav>
+  );
+}
