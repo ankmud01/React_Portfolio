056e12ff59e1725da31b0f301cf833c0e8978f78
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-25 23:31:54 -0400

    Updates

---
+++ precache-manifest.9eea6f64ad44112b005921bc38d0be71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59684522e814527214f997a897e7e9c2",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "46f4e22e112d1a8f7f4d",
+    "url": "/React_Portfolio/static/css/main.10e6b660.chunk.css"
+  },
+  {
+    "revision": "ac709a57869d555f2a72",
+    "url": "/React_Portfolio/static/js/2.7a81e1cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.7a81e1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f4e22e112d1a8f7f4d",
+    "url": "/React_Portfolio/static/js/main.cec9023b.chunk.js"
+  },
+  {
+    "revision": "1235af553f36dd27a713",
+    "url": "/React_Portfolio/static/js/runtime-main.a4a4b560.js"
+  }
+]);
