f2e97596d6e957b21fc1317a64c8f0f2055eff50
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-28 13:43:48 -0400

    fixed route issue

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { Route, withRouter } from "react-router-dom";
-import "./Container.css";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './Container.css';
 
-import About from "../Pages/About";
-import Projects from "../Pages/Projects";
-import Contact from "../Pages/Contact";
+import About from '../Pages/About';
+import Projects from '../Pages/Projects';
+import Contact from '../Pages/Contact';
+import Switch from 'react-bootstrap/esm/Switch';
 // import { Jumbotron } from "react-bootstrap";
 
 export default function Container() {
   return (
     <div className='container'>
-        <div className = 'jumbotron'>
-            <Route exact path="/" component={About} />
-            <Route exact path="/portfolio" component={Projects} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="*" component={About}/>
-        </div>
+      <div className='jumbotron'>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/portfolio' component={Projects} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/React_Portfolio' component={About} />
+        </Switch>
+      </div>
     </div>
   );
 }
